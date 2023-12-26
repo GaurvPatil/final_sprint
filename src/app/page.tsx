83b5267c32229e628fa8c4f471a24ee179658d99
GaurvPatil <gaurvpatil17@gmail.com>
2023-12-26 13:02:20 +0530

    new start

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,62 +1,9 @@
-"use client";
-
-import Stack from "@mui/material/Stack";
-import Button from "@mui/material/Button";
-import { useRouter } from "next/navigation";
-import { useSuspenseQuery } from "@apollo/experimental-nextjs-app-support/ssr";
-import { gql } from "@apollo/client";
+import Image from 'next/image'
 
 export default function Home() {
-  const query = gql`
-    query getStudents {
-      getStudents {
-        username
-        email
-      }
-    }
-  `;
-
-  const router = useRouter();
-
-  const { data } = useSuspenseQuery<{
-    getStudents: Array<{ username: string; email: string }>;
-  }>(query); //there is solution for this(codegen)
-
   return (
-    <Stack direction="row" spacing={2}>
-      <main>
-        {data?.getStudents?.map(
-          (student: { username: string; email: string }, index: number) => {
-            return (
-              <p key={index}>
-                {student.username} , {student.email}
-              </p>
-            );
-          }
-        )}
-      </main>
-
-      <Button
-        variant="outlined"
-        color="success"
-        onClick={() => router.push("/signup")}
-      >
-        Admin
-      </Button>
-      <Button
-        variant="outlined"
-        color="success"
-        onClick={() => router.push("/signup")}
-      >
-        Guid
-      </Button>
-      <Button
-        variant="outlined"
-        color="success"
-        onClick={() => router.push("/signup")}
-      >
-        Student
-      </Button>
-    </Stack>
-  );
+    <main >
+    Working !
+    </main>
+  )
 }
